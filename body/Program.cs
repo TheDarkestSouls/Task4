be2009cea862d197ad765afe3ff88bf7e979b505
NikitaP <unntiss@bk.ru>
2022-08-07 17:55:16 +0300

    body added

---
+++ body/Program.cs
@@ -0,0 +1,16 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+if(a > b)
+{
+    if(a > c) Console.Write(a);
+    else Console.Write(c);
+         
+}
+else
+{
+    if(b > c) Console.Write(b);
+    else Console.Write(c);
+         
+}
